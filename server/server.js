8ad1e2ca8e487b0c774353016737a521996de6e5
SummaDiaboli <salim.hussaini@backlinq.ng>
2019-09-21 18:23:52 +0100

    Added cors middleware

--- server/server.js
+++ server/server.js
@@ -1,19 +1,25 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
 const app = express()
+app.use(cors())
 app.use(bodyParser.json())
 // app.use(bodyParser.urlencoded({ extended: false }))
 
 const user = require('./routes/users')
 const post = require('./routes/posts')
 const message = require('./routes/messages')
+const podcast = require('./routes/podcasts')
+const notification = require('./routes/notifications')
+const friendRequest = require('./routes/friend-request')
 
 app.get('/', (req, res) => console.log("Hello world"))
 
+
 /**
- * Api for creating, updating, deleting
- * and getting users from the database
+ * API for creating, updating, deleting and getting
+ * users from the database
  */
 app.get('/api/users', user.getUsers)
 app.get('/api/users/:id', user.getUserById)
@@ -23,7 +29,7 @@ app.delete('/api/users/:id', user.deleteUser)
 
 
 /**
- * Api for getting, creating and deleting posts
+ * API for getting, creating and deleting posts
  * from the database
  */
 app.get('/api/posts', post.getPosts)
@@ -34,11 +40,40 @@ app.delete('/api/posts/:id', post.deletePosts)
 
 
 /**
- * Api for getting, deleting and creating messages
+ * API for getting, deleting and creating messages
  */
 app.get('/api/messages', message.getMessages)
+app.get('/api/messages/:id', message.getMessageById)
 app.post('/api/messages', message.createMessage)
 app.delete('/api/messages/:id', message.deleteMessage)
+
+
+/**
+ * API for getting, creating, editing and deleting podcasts
+ */
+app.get('/api/podcasts', podcast.getPodcasts)
+app.get('/api/podcasts/:id', podcast.getPodcastById)
+app.post('/api/podcasts', podcast.createPodcast)
+app.put('/api/podcasts/:id', podcast.updatePodcast)
+app.delete('/api/podcasts/:id', podcast.deletePodcast)
+
+
+/**
+ * API for getting, creating and deleting notifications
+ */
+app.get('/api/notifications', notification.getNotifications)
+app.get('/api/notifications/:id', notification.getNotificationById)
+app.post('/api/notifications', notification.createNotification)
+app.delete('/api/notifications/:id', notification.deleteNotification)
+
+
+/**
+ * API for getting, creating and deleting friend requests
+ */
+app.get('/api/friend-requests', friendRequest.getFriendRequests)
+app.get('/api/friend-requests/:id', friendRequest.getFriendRequestById)
+app.post('/api/friend-requests', friendRequest.createFriendRequest)
+app.delete('/api/friend-requests/:id', friendRequest.deleteFriendRequest)
 
 // app.listen(3001, () => console.log("Listening on port 3001"))
 module.exports = app

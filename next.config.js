17a0e094197761d63fb0cd46816954fe4f878a60
SummaDiaboli <salim.hussaini@backlinq.ng>
2019-09-23 16:24:38 +0100

    Edited next.config.js and packages

--- next.config.js
+++ next.config.js
@@ -1,10 +1,26 @@
+/* const { PHASE_PRODUCTION_SERVER } =
+    process.env.NODE_ENV === 'development'
+        ? {}
+        : !process.env.NOW_REGION
+            ? require('next/constants')
+            : require('next-server/constants'); */
+
 const withCSS = require('@zeit/next-css')
 const withImages = require('next-images')
 const withPlugins = require('next-compose-plugins')
 const withPurgeCss = require('next-purgecss')
 
-module.exports = withPlugins([
-    [withCSS],
-    [withImages],
-    [withPurgeCss]
-])
+
+if (process.env.NODE_ENV === 'development') {
+    module.exports = {}
+} else {
+    module.exports =
+        withPlugins([
+            [withCSS],
+            [withImages],
+            [withPurgeCss]
+        ])
+
+}
+
+

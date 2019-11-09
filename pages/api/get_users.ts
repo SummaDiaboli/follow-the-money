207a89e33eca2f6091a2510449e249cf39b5d2ce
SummaDiaboli <salim.hussaini@backlinq.ng>
2019-11-09 17:54:38 +0100

    Fix response to res

--- pages/api/get_users.ts
+++ pages/api/get_users.ts
@@ -20,8 +20,8 @@ export default (req: Request, res: Response) => {
 const getAllUsers = (req: Request, res: Response) => {
     pool.query('SELECT * FROM users ORDER BY username ASC', (error, results) => {
         if (error) {
-            response.status(500)
+            res.status(500)
         }
-        response.status(201).send(results.rows)
+        res.status(201).send(results.rows)
     })
 }

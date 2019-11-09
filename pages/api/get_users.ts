946ea8b2932b3bde58dd819472cccf0d37f4bcd1
SummaDiaboli <salim.hussaini@backlinq.ng>
2019-11-09 17:26:37 +0100

    Added API for fetching all users

---
+++ pages/api/get_users.ts
@@ -0,0 +1,27 @@
+import { Request, Response, response } from "express"
+
+const pool = require('../../database/queries')
+
+export default (req: Request, res: Response) => {
+    const {
+        method
+    } = req
+
+    switch (method) {
+        case 'GET':
+            getAllUsers(req, res)
+            break;
+
+        default:
+            break;
+    }
+}
+
+const getAllUsers = (req: Request, res: Response) => {
+    pool.query('SELECT * FROM users', (error, results) => {
+        if (error) {
+            response.status(500)
+        }
+        response.status(201).send(results.rows)
+    })
+}
